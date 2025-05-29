a3a5830e317e7708b3086be39f1c778c2252c957
amwang9276 <amwangpanda@gmail.com>
2025-05-28 23:10:41 -0400

    input info in terminal instead of .env

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 APOLLO_API_KEY = os.getenv("APOLLO_API_KEY")
-INPUT_PATH = os.getenv("INPUT_PATH")
-parsed_path = INPUT_PATH.split("\\")[:-1]
-parsed_path.append("emails.csv")
-
-OUTPUT_PATH = "\\".join(parsed_path)
 
 if __name__ == "__main__":
-    companies = files.read_companies_csv(INPUT_PATH) # read in companies
+    input_path = input("Enter file path: ")
+    file_name = input("Enter .csv file name (e.g. testing.csv): ")
+    
+    INPUT_PATH = input_path + "\\" + file_name
 
+    parsed_path = INPUT_PATH.split("\\")[:-1]
+    parsed_path.append("emails.csv")
+    OUTPUT_PATH = "\\".join(parsed_path)
+
+    companies = files.read_companies_csv(INPUT_PATH) # read in companies
     company_info = request.get_emails(companies) # find name and email through apollo
 
     files.write_email_csv(INPUT_PATH, OUTPUT_PATH, company_info) # write to csv file
 
-    print("Done.")
+    print(f"Results printed to emails.csv file in {str(input_path)}.")
+    
